6c09dca0e23533b3f44f1434068fb227af871665
bq-hentai27 <bq_hentai@yahoo.com>
2017-07-23 16:00:43 +0800

    fix version bug

--- src/api/index.js
+++ src/api/index.js
@@ -8,32 +8,7 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://api.caimixinli.com/serverapi/'
 
 export const getCategories = function () {
-  // return axios.get()
-  return Promise.resolve({
-    success: true,
-    msg: '成功.',
-    code: 1001,
-    data: {
-      startImg: {
-        imgUrl: 'http://pic.58pic.com/58pic/16/69/41/53G58PICn6P_1024.jpg',
-        url: ''
-      },
-      categorys: [
-        {
-          categoryName: '推荐',
-          categoryCode: 'tuijian'
-        },
-        {
-          categoryName: '恋爱',
-          categoryCode: 'lianai'
-        },
-        {
-          categoryName: '校园',
-          categoryCode: 'xiaoyuan'
-        }
-      ]
-    }
-  })
+  return axios.get('http://api.caimixinli.com/serverapi/config/init')
 }
 
 export const getEssayList = function ({ pageNo, code }) {
