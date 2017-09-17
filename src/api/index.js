48d5a5095160880d62e4f6628dee7334fb397dd1
bq-hentai27 <bq_hentai@yahoo.com>
2017-09-17 21:59:44 +0800

    fix to http-proxy mtd

--- src/api/index.js
+++ src/api/index.js
@@ -4,101 +4,146 @@
  */
 
 import axios from 'axios'
-import jsonp from 'jsonp'
-import qs from 'qs'
+// import jsonp from 'jsonp'
+// import qs from 'qs'
 import { toLogin } from '@/utils'
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://api.caimixinli.com/serverapi/'
+// axios.defaults.baseURL = 'http://api.caimixinli.com/serverapi/'
+axios.defaults.baseURL = '/serverapi'
 
 const UN_AUTH_CODE = 1022
 
-const jsonpWrapper = function (url, data) {
-  return new Promise((resolve, reject) => {
-    jsonp(axios.defaults.baseURL + url, { param: qs.stringify(data) + '&callback' }, (err, data) => {
-      if (err) {
-        reject(err)
-      }
-      if (Number(data.code) === UN_AUTH_CODE) {
-        toLogin()
-        return
-      }
-      resolve(data)
-    })
-  })
-}
+axios.interceptors.response.use(response => {
+  if (Number(response.data.code) === UN_AUTH_CODE) {
+    toLogin()
+    return
+  }
+  return response.data
+}, error => {
+  return Promise.reject(error)
+})
+
+// const jsonpWrapper = function (url, data) {
+//   return new Promise((resolve, reject) => {
+//     jsonp(axios.defaults.baseURL + url, { param: qs.stringify(data) + '&callback' }, (err, data) => {
+//       if (err) {
+//         reject(err)
+//       }
+//       if (Number(data.code) === UN_AUTH_CODE) {
+//         toLogin()
+//         return
+//       }
+//       resolve(data)
+//     })
+//   })
+// }
 
 export const loadCategories = function () {
-  // return axios.get('config/init')
-  return jsonpWrapper('config/init')
+  return axios.get('config/init')
+  // return jsonpWrapper('config/init')
 }
 
 export const loadEssayList = function ({ pageNo, code }) {
-  // return axios.get(`content/queryContent?pageNo=${pageNo}&categoryCode=${code}`)
-  return jsonpWrapper('content/queryContent', {
-    pageNo,
-    categoryCode: code
+  return axios.get('content/queryContent', {
+    params: {
+      pageNo,
+      categoryCode: code
+    }
   })
+  // return jsonpWrapper('content/queryContent', {
+  //   pageNo,
+  //   categoryCode: code
+  // })
 }
 
 export const loadStoryList = function ({ pageNo }) {
-  return jsonpWrapper('content/queryMyContent', {
-    pageNo
+  return axios.get('content/queryMyContent', {
+    params: { pageNo }
   })
+  // return jsonpWrapper('content/queryMyContent', {
+  //   pageNo
+  // })
 }
 
 export const getVerifyCode = function ({ phone }) {
-  // return axios.get(`userlogin/getPhoneCheckCode?phone=${phone}`)
-  return jsonpWrapper('userlogin/getPhoneCheckCode', {
-    phone
+  return axios.get('userlogin/getPhoneCheckCode', {
+    params: { phone }
   })
+  // return jsonpWrapper('userlogin/getPhoneCheckCode', {
+  //   phone
+  // })
 }
 
 export const loginViaVerifyCode = function ({ phone, verifyCode }) {
-  // return axios.get(`userlogin/loginNoPassword?phone=${phone}&phoneCheckCode=${verifyCode}`)
-  return jsonpWrapper(`userlogin/loginNoPassword`, {
-    phone,
-    phoneCheckCode: verifyCode
+  return axios.get('userlogin/loginNoPassword', {
+    params: {
+      phone,
+      phoneCheckCode: verifyCode
+    }
   })
+  // return jsonpWrapper(`userlogin/loginNoPassword`, {
+  //   phone,
+  //   phoneCheckCode: verifyCode
+  // })
 }
 
 export const loginViaPsw = function ({ phone, psw }) {
-  // return axios.get(`userlogin/loginPassword?phone=${phone}&password=${psw}`)
-  return jsonpWrapper(`userlogin/loginPassword`, {
-    phone,
-    password: psw
+  return axios.get('userlogin/loginPassword', {
+    params: {
+      phone,
+      password: psw
+    }
   })
+  // return jsonpWrapper(`userlogin/loginPassword`, {
+  //   phone,
+  //   password: psw
+  // })
 }
 
 export const getEssayDetail = function ({ contentId }) {
-  // return axios.get(`detail/contentDetail?contentId=${contentId}`)
-  return jsonpWrapper('detail/contentDetail', {
-    contentId
+  return axios.get('detail/contentDetail', {
+    params: { contentId }
   })
+  // return jsonpWrapper('detail/contentDetail', {
+    // contentId
+  // })
 }
 
 export const submitFollowUser = function ({ followUserId }) {
-  return jsonpWrapper('relation/followUser', {
-    followUserId
+  return axios.get('relation/followUser', {
+    params: { followUserId }
   })
+  // return jsonpWrapper('relation/followUser', {
+  //   followUserId
+  // })
 }
 
 export const likeArticle = function ({ contentId }) {
-  return jsonpWrapper('relation/likeContent', {
-    contentId
+  return axios.get('relation/likeContent', {
+    params: { contentId }
   })
+  // return jsonpWrapper('relation/likeContent', {
+  //   contentId
+  // })
 }
 
 export const collectArticle = function ({ contentId }) {
-  return jsonpWrapper('relation/collectContent', {
-    contentId
+  return axios.get('relation/collectContent', {
+    params: { contentId }
   })
+  // return jsonpWrapper('relation/collectContent', {
+  //   contentId
+  // })
 }
 
 export const likeComment = function ({ commentId }) {
-  return jsonpWrapper('relation/likeComment', {
-    comentId: commentId
+  return axios.get('relation/likeComment', {
+    params: { commentId }
   })
+  // return jsonpWrapper('relation/likeComment', {
+  //   comentId: commentId
+  // })
 }
 
 export const sendStory = function ({ title, detail, voiceUrl, imageUrls }) {
@@ -108,12 +153,20 @@ export const sendStory = function ({ title, detail, voiceUrl, imageUrls }) {
   if (!imageUrls) {
     imageUrls = ''
   }
-  return jsonpWrapper(`content/addContent`, {
-    title,
-    contentTxt: detail,
-    voiceUrl,
-    imageUrls
+  return axios.get('content/addContent', {
+    params: {
+      title,
+      contentTxt: detail,
+      voiceUrl,
+      imageUrls
+    }
   })
+  // return jsonpWrapper(`content/addContent`, {
+  //   title,
+  //   contentTxt: detail,
+  //   voiceUrl,
+  //   imageUrls
+  // })
 }
 
 export const uploadVoice = function ({ blob }) {
@@ -127,9 +180,16 @@ export const commentArticle = function ({
   contentId,
   commentContent
 }) {
-  return jsonpWrapper('comment/addComment', {
-    beCommentUserId,
-    contentId,
-    commentContent
+  return axios.get('comment/addComment', {
+    params: {
+      beCommentUserId,
+      contentId,
+      commentContent
+    }
   })
+  // return jsonpWrapper('comment/addComment', {
+  //   beCommentUserId,
+  //   contentId,
+  //   commentContent
+  // })
 }
